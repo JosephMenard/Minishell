408e9fb7fa493f008e493efe5985bf855fd32544
Joseph Menard <jmenard@made-f0Ar10s12.clusters.42paris.fr>
2024-11-14 14:16:48 +0100

    commit inutile

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jmenard <jmenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 15:09:56 by jmenard           #+#    #+#             */
-/*   Updated: 2024/11/14 14:00:51 by jmenard          ###   ########.fr       */
+/*   Updated: 2024/11/14 14:15:47 by jmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,14 +77,12 @@ bool	check_files_cmd_exist_left(t_ast *ast_list)
 	count = 0;
 	if (!ast_list->commande->cmd_args[0])
 	{
-		printf("pas de cmd left\n");
 		if (ast_list->commande->files)
 			open_files(ast_list->commande);
 		count++;
 	}
 	if (ast_list->left)
 	{
-		printf("left\n");
 		return (check_files_cmd_exist_left(ast_list->left));
 	}
 	if (count > 0)
@@ -99,14 +97,12 @@ bool	check_files_cmd_exist_right(t_ast *ast_list)
 	count = 0;
 	if (!ast_list->commande->cmd_args[0])
 	{
-		printf("pas de cmd right %s\n", ast_list->commande->files->files_name);
 		if (ast_list->commande->files)
 			open_files(ast_list->commande);
 		count++;
 	}
 	if (ast_list->right)
 	{
-		printf("right\n");
 		return (check_files_cmd_exist_right(ast_list->right));
 	}
 	if (count > 0)
